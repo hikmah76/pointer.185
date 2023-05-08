bf1b613902455e30a4b94c4154946f8740cc171c
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-08 17:31:24 +0700

    int main() {

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -12,3 +12,15 @@ void mahasiswa::showNim() { // implementasi mwthod diluar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };  // object mhs
+	mhs.showNim();      // Number Acces Operator
+
+	mahasiswa* pMhs = &mhs; // pointer reference  reFMHS
+	pMhs->nim = 3;           // arrow operator
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
+
